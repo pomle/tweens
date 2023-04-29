b88ad8dd6d1a96d59dcbe7b88c4561496e79b568
Pontus Alexander <pontus.alexander@gmail.com>
2023-04-29 21:10:07 +0200

    Provide value tween

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { spring } from './tween';
+import { spring, value } from './tween';
 import { zoom, position, lookAt, opacity, rotation, scale, vec3 } from './3';
 
 export const springs = {
@@ -10,6 +10,7 @@ export const springs = {
   vec3,
   zoom,
   spring,
+  value,
 };
 
 export type { Config as SpringConfig } from './tween';
