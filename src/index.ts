5026064b5ea8fc8d2221487e6294460f504a35fb
Pontus Alexander <pontus.alexander@gmail.com>
2023-05-01 08:45:50 +0200

    No bundled Three.js adapters

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,3 @@
-import { spring } from './spring';
-import {
-  zoom,
-  fov,
-  position,
-  lookAt,
-  opacity,
-  rotation,
-  scale,
-  vec3,
-} from './3';
-
-export const springs = {
-  lookAt,
-  fov,
-  opacity,
-  position,
-  rotation,
-  scale,
-  vec3,
-  zoom,
-  spring,
-};
+export { spring } from './spring';
 
 export type { Config as SpringConfig, Spring } from './spring';
