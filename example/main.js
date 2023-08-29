d4e5eae9a7230b70048db08d95eab474c8111707
Pontus Alexander <pontus.alexander@gmail.com>
2023-08-29 20:08:19 +0200

    Provide example

---
+++ example/main.js
@@ -0,0 +1,145 @@
+import { spring } from './tween.js';
+
+class Box {
+  x = 0;
+  y = 0;
+  w = 64;
+  h = 64;
+}
+
+class Circle {
+  x = 0;
+  y = 0;
+  r = 16;
+}
+
+function createEngine(canvas) {
+  const context = canvas.getContext('2d');
+
+  const box = new Box();
+  const anchor = new Circle();
+
+  const tween = spring({ x: 0, y: 0 });
+
+  function moveTo(x, y) {
+    x = Math.round(x);
+    y = Math.round(y);
+
+    anchor.x = x;
+    anchor.y = y;
+
+    tween.to({
+      x,
+      y,
+    });
+  }
+
+  function handlePointer(event) {
+    if (event.buttons === 1) {
+      moveTo(event.offsetX, event.offsetY);
+    }
+  }
+
+  moveTo(canvas.width / 2, canvas.height / 2);
+
+  canvas.addEventListener('pointermove', handlePointer);
+  canvas.addEventListener('pointerdown', handlePointer);
+
+  const config = {
+    stiffness: NaN,
+    mass: NaN,
+    friction: NaN,
+    precision: 0.01,
+  };
+
+  function configure(input) {
+    const name = input.name;
+    const value = parseFloat(input.value);
+
+    config[name] = value;
+
+    console.log(config);
+
+    tween.reconfigure(config);
+
+    output();
+  }
+
+  let lastTime = 0;
+  let deltaTime = 0;
+
+  function update(time) {
+    deltaTime = (time - lastTime) / 1000;
+
+    if (tween.update(deltaTime)) {
+      box.x = tween.value.x;
+      box.y = tween.value.y;
+
+      draw();
+
+      output();
+    }
+
+    lastTime = time;
+
+    window.requestAnimationFrame(update);
+  }
+
+  function output() {
+    const code = `const config = {
+  friction: ${config.friction},
+  mass: ${config.mass},
+  stiffness: ${config.stiffness},
+  precision: ${config.precision},
+};
+
+const position = spring({x: 0, y: 0}, config);
+position.to({
+  x: ${anchor.x.toFixed(2)},
+  y: ${anchor.y.toFixed(2)},
+});
+`;
+
+    document.getElementById('code').textContent = code;
+
+    const console = `Position
+X: ${box.x}
+Y: ${box.y}
+`;
+
+    document.getElementById('console').textContent = console;
+  }
+
+  document.querySelectorAll('.config').forEach((input) => {
+    input.addEventListener('input', (event) => {
+      configure(event.target);
+    });
+
+    configure(input);
+  });
+
+  function draw() {
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    context.fillStyle = '#000';
+    context.fillRect(box.x - box.w / 2, box.y - box.h / 2, box.w, box.h);
+
+    context.beginPath();
+    context.moveTo(box.x, box.y);
+    context.lineTo(anchor.x, anchor.y);
+    context.strokeStyle = '#888';
+    context.stroke();
+
+    context.beginPath();
+    context.arc(anchor.x, anchor.y, anchor.r, 0, 2 * Math.PI, false);
+    context.fillStyle = '#f00';
+    context.fill();
+  }
+
+  window.requestAnimationFrame(update);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const canvas = document.querySelector('canvas');
+  createEngine(canvas);
+});
